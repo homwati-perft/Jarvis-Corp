1a3785e262767490a0ffb798d6357fef1d729950
ankit.tawale <ankit.tawale@perficient.com>
2020-03-06 11:15:52 +0530

    Updated tile component in which js is not working properly so

--- Jarvis-corp/JavaScript/Main.js
+++ Jarvis-corp/JavaScript/Main.js
@@ -5,26 +5,12 @@
 function closeSearch() {
     document.getElementById("myOverlay").style.display = "none";
 }
-function load() {
-    //document.getElementById("loadbox").style.display = "none";
-    document.getElementsByClassName("morebox").style.display = "none";
-}
+//function load() {
+//    //document.getElementById("loadbox").style.display = "none";
+//    document.getElementsByClassName("morebox").style.display = "none";
+//}
 
-function loadmore() {
-    document.getElementById("loadbox").style.display = "block";
-    document.getElementById("loadMore").style.display = "none";
-}
-
-//$(document).ready(function () {
-//    $(".morebox").slice(0, 3).show();
-//    if ($(".general:hidden").length != 0) {
-//        $("#loadMore").show();
-//    }
-//    $("#loadMore").on('click', function (e) {
-//        e.preventDefault();
-//        $(".morebox:hidden").slice(0, 6).slideDown();
-//        if ($(".morebox:hidden").length == 0) {
-//            $("#loadMore").fadeOut('slow');
-//        }
-//    });
-//});
+//function loadmore() {
+//    document.getElementById("loadbox").style.display = "block";
+//    document.getElementById("loadMore").style.display = "block";
+//}
