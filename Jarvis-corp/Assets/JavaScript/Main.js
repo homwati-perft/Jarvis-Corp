805ee88a225b30c50ff5eb7788bc6d614907d576
homwati.pawar <homwati.pawar@ZIL215>
2020-03-11 16:16:52 +0530

    Stage changes of service component

--- Jarvis-corp/Assets/JavaScript/Main.js
+++ Jarvis-corp/Assets/JavaScript/Main.js
@@ -6,11 +6,6 @@ function closeSearch() {
     document.getElementById("myOverlay").style.display = "none";
 }
 
-function abc() {
-    alert("called");
-    console.log("bdmsvdhsvdhsdhjgsdjh>>>>");
-}
-
 $(document).ready(function () {
     $("#test .tab-pane").hide();
     $("#test .tab-pane:first").show();
@@ -20,10 +15,10 @@ function onLoad() {
     console.log("finally calleds")
 }
 function Test(clicked) {
-    console.log("dhgkjhdsgfhsghfgdkhsgfhkbvdsjfgkj")
     var els = document.getElementsByClassName("tab-pane");
     var myFunc = function myFunction(item) {
         if (item.id == "comp-" + clicked) {
+            console.log("item.id>>>>", item.id)
             document.getElementById(item.id).style.display = "block";
         } else {
             document.getElementById(item.id).style.display = "none";
